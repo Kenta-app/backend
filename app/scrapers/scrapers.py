bbd5762426ac22dec3eece821fd62e7a92a5a92e
Salvador Diaz <sdiaz.aguirre2003@gmail.com>
2026-01-19 00:06:50 -0500

    feat: logic for ElComercio

--- app/scrapers/scrapers.py
+++ app/scrapers/scrapers.py
@@ -2,6 +2,8 @@ from app.scrapers.base_scraper import BaseScraper
 from typing import List, Dict
 from datetime import datetime
 import logging
+from zoneinfo import ZoneInfo
+
 logger = logging.getLogger(__name__)
 class BBCNewsScraper(BaseScraper):
     def __init__(self):
@@ -46,7 +48,7 @@ class BBCNewsScraper(BaseScraper):
         return articles
 class ElComercioScraper(BaseScraper):
     def __init__(self):
-        super().__init__("El Comercio", "https://elcomercio.pe/politica/jose-jeri-dice-que-ministro-del-interior-lo-acompano-a-chifa-y-ofrece-disculpas-publicas-por-ingresar-encapuchado-noticia/")
+        super().__init__("El Comercio", "https://elcomercio.pe/politica/")
 
     def scrape(self) -> List[Dict]:
         """Ejemplo de scraper para El Comercio (adaptar según estructura real)"""
@@ -54,23 +56,49 @@ class ElComercioScraper(BaseScraper):
         try:
             soup = self.fetch_page(self.base_url)
 
-            title_elem = soup.select_one('h1')
-            summary_elem = soup.select_one('h2')
-            contents=soup.select('p.story-contents__font-paragraph')
-            published_date=soup.select_one('time')
-            content_text = " ".join([self.clean_text(p.get_text()) for p in contents])
+            containers=soup.select('div.story-item')[:13]
 
-            if title_elem:
-                article = {
-                    'title': self.clean_text(title_elem.get_text()),
-                    'url': self.base_url,
-                    'summary': self.clean_text(summary_elem.get_text()) if summary_elem else None,
-                    'content': content_text,
-                    'source': self.source_name,
-                    'published_date': published_date,
-                    'scraped_date': datetime.now()
-                }
-                articles.append(article)
+            print(len(containers))
+
+            for container in containers:
+
+                fecha_html=container.select_one('.story-item__date-time').text.strip()
+                fecha_html_date = datetime.strptime(fecha_html, "%d/%m/%Y").date()
+                fecha_actual_date = datetime.now(ZoneInfo("America/Lima")).date()
+
+
+                if fecha_html_date != fecha_actual_date:
+                    continue
+                link= container.select_one('a.story-item__title')
+
+                article_url=link.get('href')
+                if not article_url.startswith('http'):
+                    article_url = f"https://elcomercio.pe{article_url}"
+                article_soup=self.fetch_page(article_url)
+
+                if article_soup:
+                    if article_soup.select_one('div.story-header-headsubscription__text')=="Solo para suscriptores":
+                        continue
+
+                title_elem = article_soup.select_one('h1.sht__title')
+                summary_elem = article_soup.select_one('h2.sht__summary')
+
+                contents=article_soup.select('p.story-contents__font-paragraph')
+
+                content_text = " ".join([self.clean_text(p.get_text()) for p in contents])
+                author=article_soup.select_one('a.s-aut__n')
+                if title_elem:
+                    article = {
+                        'title': self.clean_text(title_elem.get_text()),
+                        'url': article_url,
+                        'summary': self.clean_text(summary_elem.get_text()) if summary_elem else None,
+                        'content': content_text,
+                        'source': self.source_name,
+                        'published_date': fecha_html_date,
+                        'scraped_date': fecha_actual_date,
+                        'author':author
+                    }
+                    articles.append(article)
 
         except Exception as e:
             logger.error(f"Error scraping {self.source_name}: {str(e)}")
--- app/scrapers/test.py
+++ app/scrapers/test.py
@@ -3,4 +3,8 @@ from app.scrapers.scrapers import BBCNewsScraper, ElComercioScraper
 scraper=ElComercioScraper()
 articles=scraper.scrape()
 print(len(articles))
-print(articles[0])
+print(articles[2])
+
+#from zoneinfo import ZoneInfo
+#from datetime import datetime
+#print(datetime.now(ZoneInfo("America/Lima")).date())
